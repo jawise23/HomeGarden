87106550976b64f10df697b05586ea5098368f53
James Wise <jawise@icloud.com>
2023-12-12 19:31:34 -0500

    Further refactoring of the temp_humid_sensor script

--- sensors/temp_humid_sensor.py
+++ sensors/temp_humid_sensor.py
@@ -1,42 +1,52 @@
 import pandas as pd
 import time
+import logging
 from sensors.dht22_sensor import read_dht22_sensor
 
-def collect_sensor_data(sensor_data, sleep_time=30):
+def collect_dht22_sensor_data(number_of_readings, sleep_time=30):
     """
-    Collects data from the DHT22 sensor and returns the updated DataFrame.
+    Collects data from the DHT22 sensor for a specified number of readings.
 
     Args:
-        sensor_data (DataFrame): The DataFrame to append new sensor data.
-        sleep_time (int): The time in seconds to sleep after each reading.
+        number_of_readings (int): Number of sensor readings to collect.
+        sleep_time (int): Time in seconds to sleep after a successful reading.
 
     Returns:
-        DataFrame: Updated DataFrame with new sensor readings.
+        DataFrame: DataFrame with Timestamp, Temperature, and Humidity.
 
-    Note:
-        - If the sensor read fails, the failure is logged and no data is appended.
+    Raises:
+        sensor_reading_exceptions: Specific exceptions related to sensor reading.
     """
-    try:
-        temperature, humidity = read_dht22_sensor()
-        if temperature is not None and humidity is not None:
-            timestamp = pd.Timestamp.now()
-            sensor_data = sensor_data.append(
-                {'Timestamp': timestamp, 'Temperature': temperature, 'Humidity': humidity},
-                ignore_index=True
-            )
-    except Exception as e:
-        print(f"Error reading sensor: {e}")
+    data = []
+    for _ in range(number_of_readings):
+        try:
+            temperature, humidity = read_dht22_sensor()
+            if temperature is not None and humidity is not None:
+                timestamp = pd.Timestamp.now()
+                data.append({'Timestamp': timestamp, 'Temperature': temperature, 'Humidity': humidity})
+                time.sleep(sleep_time)
+        except sensor_reading_exceptions as e:  # Replace with specific exceptions
+            logging.error(f"Error reading sensor: {e}")
 
-    time.sleep(sleep_time)
-    return sensor_data
+    return pd.DataFrame(data, columns=['Timestamp', 'Temperature', 'Humidity'])
 
-# Initialize DataFrame
-data_columns = ['Timestamp', 'Temperature', 'Humidity']
-sensor_data = pd.DataFrame(columns=data_columns)
+def save_sensor_data_to_csv(sensor_data, file_path):
+    """
+    Saves sensor data to a CSV file.
 
-# Collect data
-for _ in range(number_of_readings):  # Replace with a suitable condition
-    sensor_data = collect_sensor_data(sensor_data)
+    Args:
+        sensor_data (DataFrame): Data to save.
+        file_path (str): Path to save the CSV file.
+    """
+    sensor_data.to_csv(file_path, index=False)
 
-# Save to CSV
-sensor_data.to_csv('path_to_save/sensor_data.csv', index=False)
+# Initialize Logging
+logging.basicConfig(level=logging.INFO)
+
+# Parameters
+number_of_readings = 10  # Define or pass as a parameter
+file_path = 'path_to_save/sensor_data.csv'  # Define or pass as a parameter
+
+# Collect and Save Data
+sensor_data = collect_dht22_sensor_data(number_of_readings)
+save_sensor_data_to_csv(sensor_data, file_path)

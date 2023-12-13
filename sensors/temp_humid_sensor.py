aa9aa1eb8eda2554234a5bbec4965044c17f8546
James Wise <jawise@icloud.com>
2023-12-12 19:46:56 -0500

    Last refactor for tonight

--- sensors/temp_humid_sensor.py
+++ sensors/temp_humid_sensor.py
@@ -3,6 +3,9 @@ import time
 import logging
 from sensors.dht22_sensor import read_dht22_sensor
 
+# Configure logging at the start of your main application
+logging.basicConfig(level=logging.INFO)
+
 def collect_dht22_sensor_data(number_of_readings, sleep_time=30, log_level=logging.INFO):
     """
     Collects data from the DHT22 sensor for a specified number of readings.
@@ -10,7 +13,6 @@ def collect_dht22_sensor_data(number_of_readings, sleep_time=30, log_level=loggi
     Args:
         number_of_readings (int): Number of sensor readings to collect.
         sleep_time (int): Time in seconds to sleep after a successful reading.
-        log_level (int): Logging level.
 
     Returns:
         DataFrame: DataFrame with Timestamp, Temperature, and Humidity.
@@ -23,7 +25,6 @@ def collect_dht22_sensor_data(number_of_readings, sleep_time=30, log_level=loggi
     if not isinstance(sleep_time, int) or sleep_time <= 0:
         raise ValueError("sleep_time must be a positive integer")
 
-    logging.basicConfig(level=log_level)
     data = []
     consecutive_errors = 0
     max_consecutive_errors = 5  # Configurable
@@ -38,7 +39,7 @@ def collect_dht22_sensor_data(number_of_readings, sleep_time=30, log_level=loggi
                 time.sleep(sleep_time)
             else:
                 raise ValueError("Sensor returned None values")
-        except Exception as e:  # Replace with specific exceptions from the sensor
+        except SpecificSensorException as e:  # Replace with the specific exceptions thrown by your sensor
             logging.error(f"Error reading sensor: {e}")
             consecutive_errors += 1
             if consecutive_errors >= max_consecutive_errors:
@@ -47,19 +48,25 @@ def collect_dht22_sensor_data(number_of_readings, sleep_time=30, log_level=loggi
 
     return pd.DataFrame(data, columns=['Timestamp', 'Temperature', 'Humidity'])
 
-def save_sensor_data_to_csv(sensor_data, file_path):
+def save_sensor_data_to_csv(sensor_data, file_path, chunk_size=1000):
     """
-    Saves sensor data to a CSV file.
+    Saves sensor data to a CSV file, potentially in chunks for large datasets.
 
     Args:
         sensor_data (DataFrame): Data to save.
         file_path (str): Path to save the CSV file.
+        chunk_size (int): Size of each chunk for writing large datasets.
     """
-    sensor_data.to_csv(file_path, index=False)
+    if len(sensor_data) <= chunk_size:
+        sensor_data.to_csv(file_path, index=False)
+    else:
+        for i in range(0, len(sensor_data), chunk_size):
+            sensor_data[i:i+chunk_size].to_csv(file_path if i==0 else file_path.replace('.csv', f'_{i}.csv'), 
+                                               index=False, mode='w' if i==0 else 'a', header=i==0)
 
-# Usage example
+# Example usage
 number_of_readings = 10  # Define or pass as a parameter
 file_path = 'path_to_save/sensor_data.csv'  # Define or pass as a parameter
 
-sensor_data = collect_dht22_sensor_data(number_of_readings, log_level=logging.INFO)
+sensor_data = collect_dht22_sensor_data(number_of_readings)
 save_sensor_data_to_csv(sensor_data, file_path)

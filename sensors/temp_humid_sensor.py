29a00049db5ea3b332fdc5d910da0e7995db11ae
James Wise <jawise@icloud.com>
2023-12-12 19:36:16 -0500

    More refactoring of the script

--- sensors/temp_humid_sensor.py
+++ sensors/temp_humid_sensor.py
@@ -3,30 +3,47 @@ import time
 import logging
 from sensors.dht22_sensor import read_dht22_sensor
 
-def collect_dht22_sensor_data(number_of_readings, sleep_time=30):
+def collect_dht22_sensor_data(number_of_readings, sleep_time=30, log_level=logging.INFO):
     """
     Collects data from the DHT22 sensor for a specified number of readings.
 
     Args:
         number_of_readings (int): Number of sensor readings to collect.
         sleep_time (int): Time in seconds to sleep after a successful reading.
+        log_level (int): Logging level.
 
     Returns:
         DataFrame: DataFrame with Timestamp, Temperature, and Humidity.
 
     Raises:
-        sensor_reading_exceptions: Specific exceptions related to sensor reading.
+        ValueError: If number_of_readings or sleep_time is not a positive integer.
     """
+    if not isinstance(number_of_readings, int) or number_of_readings <= 0:
+        raise ValueError("number_of_readings must be a positive integer")
+    if not isinstance(sleep_time, int) or sleep_time <= 0:
+        raise ValueError("sleep_time must be a positive integer")
+
+    logging.basicConfig(level=log_level)
     data = []
+    consecutive_errors = 0
+    max_consecutive_errors = 5  # Configurable
+
     for _ in range(number_of_readings):
         try:
             temperature, humidity = read_dht22_sensor()
             if temperature is not None and humidity is not None:
-                timestamp = pd.Timestamp.now()
+                timestamp = pd.Timestamp.now(tz='UTC')  # Timezone aware
                 data.append({'Timestamp': timestamp, 'Temperature': temperature, 'Humidity': humidity})
+                consecutive_errors = 0
                 time.sleep(sleep_time)
-        except sensor_reading_exceptions as e:  # Replace with specific exceptions
+            else:
+                raise ValueError("Sensor returned None values")
+        except Exception as e:  # Replace with specific exceptions from the sensor
             logging.error(f"Error reading sensor: {e}")
+            consecutive_errors += 1
+            if consecutive_errors >= max_consecutive_errors:
+                logging.error("Maximum consecutive errors reached, skipping further readings")
+                break
 
     return pd.DataFrame(data, columns=['Timestamp', 'Temperature', 'Humidity'])
 
@@ -40,13 +57,9 @@ def save_sensor_data_to_csv(sensor_data, file_path):
     """
     sensor_data.to_csv(file_path, index=False)
 
-# Initialize Logging
-logging.basicConfig(level=logging.INFO)
-
-# Parameters
+# Usage example
 number_of_readings = 10  # Define or pass as a parameter
 file_path = 'path_to_save/sensor_data.csv'  # Define or pass as a parameter
 
-# Collect and Save Data
-sensor_data = collect_dht22_sensor_data(number_of_readings)
+sensor_data = collect_dht22_sensor_data(number_of_readings, log_level=logging.INFO)
 save_sensor_data_to_csv(sensor_data, file_path)
